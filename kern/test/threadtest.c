fa1e1b328ffadd00fa20973e18a9275a3cf465d7
IsraelF6 <IF12B@my.fsu.edu>
2017-10-29 22:20:33 -0400

    Update threadtest.c
    fixed threadtest4

--- kern/test/threadtest.c
+++ kern/test/threadtest.c
@@ -154,16 +154,6 @@ threadtest4(int nargs, char **args)
 	init_sem();
 	kprintf("Starting thread test 4...\n");
 	
-	char name[16];
-	int i=0;
-
-	snprintf(name, sizeof(name), "threadtest%d", i);
-	/*result = thread_fork(name, NULL,
-		     doloud ? loudthread : quietthread, NULL, i);
-	if (result) {
-		panic("threadtest: thread_fork failed %s)\n", strerror(result));
-		}
-	*/
 	runthreads(0);
 	thread_join();
 	kprintf("\nThread test 4 done.\n");
